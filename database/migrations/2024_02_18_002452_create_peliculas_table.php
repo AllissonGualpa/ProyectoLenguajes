319721eae3ccdcee493225ed0c7042fe007d5adf
Allisson <allissongualpa@gmail.com>
2024-02-18 13:21:10 -0500

    Ptoyecto Lenguajes de Programacion

---
+++ database/migrations/2024_02_18_002452_create_peliculas_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('peliculas', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('nombreP', 50);
+            $table->string('anioP', 4);
+            $table->string('descriP', 100);
+            $table->unsignedBigInteger('categoria_id');
+            $table->foreign('categoria_id')->references('id')->on('categorias');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('peliculas');
+    }
+};
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+</head>
+
+<body>
+<h1>Trabajo Crud</h1>
+
+<table class="table">
+  <thead>
+    <tr>
+      <th scope="col"></th>
+      <th scope="col">Categoria</th>
+      
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      
+      <td>Categorias</td>
+      <td><button>Seleccion</button> <a href="/categorias">hola</a></td>
+    </tr>
+    <tr>
+      <th scope="row">2</th>
+     
+      <td>Comedia</td>
+      <td><button>Seleccion</button></td>
+      
+    </tr>
+    <tr>
+      <th scope="row">3</th>
+      <td>Ciencia F</td>
+      <td><button>Seleccion</button></td>
+    </tr>
+  </tbody>
+</table>
+
+</body>
+
+</html>
